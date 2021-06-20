22be6ded7ea0d9c62f19992cc5d0510d466ab986
songyi4669 <songyi4669@gmail.com>
2021-06-20 14:10:59 +0900

    add card

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './maker.module.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
@@ -7,6 +7,41 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'Ellie',
+            company: 'samsung',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL:'.aa'
+        },
+        {
+            id: '2',
+            name: 'Ellie2',
+            company: 'samsung',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL:'.aa'
+        },
+        {
+            id: '3',
+            name: 'Ellie3',
+            company: 'samsung',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL:'.aa'
+        }
+    ]);
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards}/>
+                <Preview cards={cards}/>
             </div>
             <Footer />
         </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './preview.module.css';
+import Card from '../card/card';
 
-const Preview = props =>(
+const Preview = ({cards}) =>(
     <section className={styles.preview}>
-    <h1 className={styles.title}>Card Preview</h1>
+        <h1 className={styles.title}>Card Preview</h1>
+        {
+            cards.map(card =>(
+                <Card card={card} />
+            ))}
 </section>
 );
 
